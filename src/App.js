6f3f924ab838954fb697af9e2615881368206619
MeridithJunk <Meridith.a.junk@accenture.com>
2021-01-12 20:42:55 -0600

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <main id="App">
+        <h2>HELLO! THIS WILL BE THE APP TWO! YAY! 😃</h2>
+      </main>
+  );
+}
+
+export default App;
